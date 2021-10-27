b5b4d9ff1eab3217dbf1cfad5f44325a8ef6334b
daniel.zagozdon <daniel.zagozdon@intive.com>
2021-10-27 21:12:23 +0200

    added browsers

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.opera import OperaDriverManager
 
 from Pages.Login.LoginPage import LoginPage
 from Pages.Inventory.InventoryPage import InventoryPage
@@ -14,11 +16,19 @@ web_driver = None
 def init_driver(request):
     global web_driver
     options = webdriver.ChromeOptions()
-    options.headless = True
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
     if request.param == "chrome":
         web_driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     if request.param == "firefox":
+        from selenium.webdriver.firefox.options import Options
+        options = Options()
+        options.add_argument("--headless")
         web_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    if request.param == "edge":
+        web_driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    if request.param == "opera":
+        web_driver = webdriver.Opera(executable_path=OperaDriverManager().install())
     web_driver.delete_all_cookies()
     request.cls.driver = web_driver
     request.cls.loginPage = LoginPage(web_driver)
@@ -26,4 +36,4 @@ def init_driver(request):
     request.cls.sideBarPage = SideBarPage(web_driver)
 
     yield
-    web_driver.close()
+    web_driver.quit()
